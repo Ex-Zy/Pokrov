4d119aba6f78d58239d2f22b1823cc9616daef28
Ex-Zy <serega1905@ukr.net>
2016-06-10 18:38:09 +0300

    add slider

--- src/js/common.js
+++ src/js/common.js
@@ -1,5 +1,26 @@
 $(document).ready(function() {
 
-	// go js
+	$('.js-slider-text').slick({
+		dots: false,
+		infinite: true,
+		speed: 1200,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		fade: true,
+		cssEase: 'linear',
+		asNavFor: '.js-slider-pic',
+		touchMove: false,
+	});
+
+	$('.js-slider-pic').slick({
+		dots: false,
+		arrows: false,
+		infinite: true,
+		speed: 1200,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		asNavFor: '.js-slider-text',
+		touchMove: false,
+	});
 
 });
