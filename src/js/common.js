3f8fbbbaead820a7884a8c0960b29ed634349347
Ex-Zy <serega1905@ukr.net>
2016-06-12 22:47:38 +0300

    add new popups

--- src/js/common.js
+++ src/js/common.js
@@ -23,27 +23,40 @@ $(document).ready(function() {
 		touchMove: false,
 	});
 
-	$('.js-slider-pop-text').slick({
-		dots: false,
-		infinite: true,
-		speed: 1200,
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		fade: true,
-		cssEase: 'linear',
-		asNavFor: '.js-slider-pic',
-		touchMove: false,
-	});
+	$(".js-open-popup").on("click", function(){
 
-	$('.js-slider-pop-pic').slick({
-		dots: false,
-		arrows: false,
-		infinite: true,
-		speed: 1200,
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		asNavFor: '.js-slider-text',
-		touchMove: false,
-	});
+		var link  = $(this).data('link'),
+		    popup = $('.js-popup[data-popup="' + link + '"]');
 
+		$("body").addClass('is-hidden');
+
+		popup.addClass('is-active');
+
+		$('.js-slider-pop-pic').on('init', function (){}).slick({
+			dots: false,
+			arrows: false,
+			infinite: true,
+			speed: 1200,
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			asNavFor: '.js-slider-text',
+		});
+
+		$('.js-slider-pop-text').on('init', function (){}).slick({
+			dots: false,
+			infinite: true,
+			speed: 1200,
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			fade: true,
+			cssEase: 'linear',
+			asNavFor: '.js-slider-pic',
+		});
+
+		return false;
+	});
+	$(".js-close").on("click", function(){
+		$(this).parents(".js-popup").removeClass('is-active');
+		$("body").removeClass('is-hidden');
+	});
 });
