c01e1077e63e46f3658977bf0edf9cc13e7f283e
Ex-Zy <serega1905@ukr.net>
2016-06-12 11:32:22 +0300

    add popup

--- src/js/common.js
+++ src/js/common.js
@@ -23,4 +23,27 @@ $(document).ready(function() {
 		touchMove: false,
 	});
 
+	$('.js-slider-pop-text').slick({
+		dots: false,
+		infinite: true,
+		speed: 1200,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		fade: true,
+		cssEase: 'linear',
+		asNavFor: '.js-slider-pic',
+		touchMove: false,
+	});
+
+	$('.js-slider-pop-pic').slick({
+		dots: false,
+		arrows: false,
+		infinite: true,
+		speed: 1200,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		asNavFor: '.js-slider-text',
+		touchMove: false,
+	});
+
 });
