43c94284377f6dfdfb4edf90ff463b5ae860cf3f
Sergey Nagornykh <serega1905@ukr.net>
2024-07-25 11:51:47 +0300

    fix broken url to svg image

--- src/js/common.js
+++ src/js/common.js
@@ -105,14 +105,14 @@
 			draggable: true,
 			scrollwheel: false,
 			styles: [{"featureType":"water","elementType":"geometry","stylers":[{"color":"#e9e9e9"},{"lightness":17}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffffff"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#ffffff"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":16}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":21}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#dedede"},{"lightness":21}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#ffffff"},{"lightness":16}]},{"elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#333333"},{"lightness":40}]},{"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#f2f2f2"},{"lightness":19}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#fefefe"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#fefefe"},{"lightness":17},{"weight":1.2}]}],
-			
+
 		});
 
 		var marker = new google.maps.Marker({
 			position: {lat: 49.407452, lng: 32.035812},
 			map: map,
 			title: 'Click to zoom!',
-			icon: 'img/svg/mark_icon.svg'
+			icon: 'img/svg-copy/mark_icon.svg'
 		});
 
 
@@ -137,6 +137,6 @@
 // // content-width
 // 	if ( $(window).width() > 960 ) {
 // 		var halfWidth = 480;
-// 		$('.technology__content').outerWidth(halfWidth);	
+// 		$('.technology__content').outerWidth(halfWidth);
 // 	}
 });
