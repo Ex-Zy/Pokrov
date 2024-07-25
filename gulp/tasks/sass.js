2a50320bedbf7ee94c995e06ae61c0075193227a
Sergey Nagornykh <serega1905@ukr.net>
2024-07-25 11:45:09 +0300

    fix gulp sass build task

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -1,16 +1,44 @@
 var gulp = require('gulp');
 var postcss = require('gulp-postcss');
-var sass = require('gulp-ruby-sass');
+var sass = require('gulp-sass');;
 var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('autoprefixer');
 var notify = require('gulp-notify');
 var mqpacker = require("css-mqpacker");
 var config = require('../config');
 
+sass.compiler = require('node-sass');
 
-gulp.task('sass', function() {
+// gulp.task('sass', function() {
+//
+//     var processors = [
+//         autoprefixer({browsers: ['last 10 versions'], cascade: false}),
+//         mqpacker({
+//             sort: function (a, b) {
+//                 a = a.replace(/\D/g,'');
+//                 b = b.replace(/\D/g,'');
+//                 return b-a;
+//                 // replace this with a-b for Mobile First approach
+//             }
+//         })
+//     ];
+//
+//     return sass(config.src.sass+'*.sass', {
+//         sourcemap: true,
+//         style: 'compact',
+//         emitCompileError: true
+//     })
+//     .on('error', notify.onError({
+//         title: 'Sass Error!',
+//         message: '<%= error.message %>'
+//     }))
+//     .pipe(postcss(processors))
+//     .pipe(sourcemaps.write('./'))
+//     .pipe(gulp.dest(config.dest.css));
+// });
 
-    var processors = [
+gulp.task('sass', function () {
+  var processors = [
         autoprefixer({browsers: ['last 10 versions'], cascade: false}),
         mqpacker({
             sort: function (a, b) {
@@ -22,18 +50,19 @@ gulp.task('sass', function() {
         })
     ];
 
-    return sass(config.src.sass+'*.sass', {
-        sourcemap: true,
-        style: 'compact',
-        emitCompileError: true
-    })
-    .on('error', notify.onError({
-        title: 'Sass Error!',
-        message: '<%= error.message %>'
-    }))
-    .pipe(postcss(processors))
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest(config.dest.css));
+    return gulp.src(config.src.sass+'*.sass',)
+      .pipe(sass({
+          sourcemap: true,
+          style: 'compact',
+          emitCompileError: true
+      }))
+      .on('error', notify.onError({
+          title: 'Sass Error!',
+          message: '<%= error.message %>'
+      }))
+      .pipe(postcss(processors))
+      .pipe(sourcemaps.write('./'))
+      .pipe(gulp.dest(config.dest.css));
 });
 
 gulp.task('sass:watch', function() {
